7f9efb7a104664d42a5850726248452ddbbfa56b
Christopher Edwin Mtoi <135335764+Chriss875@users.noreply.github.com>
2026-02-12 12:07:47 +0300

    Initial commit

---
+++ src/main/java/org/udsm/udsm_hackathon2026/UdsmHackathon2026Application.java
@@ -0,0 +1,12 @@
+package org.udsm.udsm_hackathon2026;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdsmHackathon2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdsmHackathon2026Application.class, args);
+    }
+
+}
